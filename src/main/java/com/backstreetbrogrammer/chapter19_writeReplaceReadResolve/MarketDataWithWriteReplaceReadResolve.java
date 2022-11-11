8bec6738199139f30852082b77fce428d386b666
Rishi Srivastava <rishisrivasta@gmail.com>
2022-11-11 20:55:08 +0800

    updated document

--- src/main/java/com/backstreetbrogrammer/chapter19_writeReplaceReadResolve/MarketDataWithWriteReplaceReadResolve.java
+++ src/main/java/com/backstreetbrogrammer/chapter19_writeReplaceReadResolve/MarketDataWithWriteReplaceReadResolve.java
@@ -22,7 +22,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
         return securityId;
     }
 
-    public void setSecurityId(String securityId) {
+    public void setSecurityId(final String securityId) {
         this.securityId = securityId;
     }
 
@@ -30,7 +30,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
         return time;
     }
 
-    public void setTime(long time) {
+    public void setTime(final long time) {
         this.time = time;
     }
 
@@ -38,7 +38,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
         return open;
     }
 
-    public void setOpen(double open) {
+    public void setOpen(final double open) {
         this.open = open;
     }
 
@@ -46,7 +46,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
         return high;
     }
 
-    public void setHigh(double high) {
+    public void setHigh(final double high) {
         this.high = high;
     }
 
@@ -54,7 +54,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
         return low;
     }
 
-    public void setLow(double low) {
+    public void setLow(final double low) {
         this.low = low;
     }
 
@@ -62,7 +62,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
         return close;
     }
 
-    public void setClose(double close) {
+    public void setClose(final double close) {
         this.close = close;
     }
 
@@ -70,7 +70,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
         return last;
     }
 
-    public void setLast(double last) {
+    public void setLast(final double last) {
         this.last = last;
     }
 
@@ -78,7 +78,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
         return isLevelOne;
     }
 
-    public void setLevelOne(boolean levelOne) {
+    public void setLevelOne(final boolean levelOne) {
         isLevelOne = levelOne;
     }
 
@@ -108,6 +108,7 @@ public class MarketDataWithWriteReplaceReadResolve implements Serializable {
 
     private Object writeReplace() throws ObjectStreamException {
         System.out.println("In writeReplace()");
+
         final var marketData = new MarketDataWithWriteReplaceReadResolve();
         marketData.setSecurityId("META");
         marketData.setTime(20000L);
